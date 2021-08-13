3478a106dd02261735dbe0679197b06478e6ae77
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2021-08-13 20:20:20 +0530

    JS appendChild

--- website/script/app.js
+++ website/script/app.js
@@ -4,27 +4,47 @@ let baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
 let finalApiKey = `&appid=${apiKey}`;
 let cityNameValue = "";
 
-// Variable Decleration
-let inputName = document.querySelector("#name")
-let generateButton = document.querySelector('button');
-let tempText = document.querySelector('.main');
+// Constant Selectors
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+const inputName = document.querySelector("#name")
+const generateButton = document.querySelector('button');
+const mainTemp = document.querySelector('.mainTemp');
+const others = document.querySelector('.others')
+const minTemp = document.querySelector('.minTemp')
+const maxTemp = document.querySelector('.maxTemp')
+const humidity = document.querySelector('.humidity')
 
-// Events and Functions
+// Variable Decleration
+let d = new Date();
+let date = `${d.getDate()} ${monthNames[d.getMonth()]},${d.getFullYear()}`;
+
+let temp = document.createElement('H1')
+temp.setAttribute('class', 'mainTemperature');
+
+// Events and Function Calls
 inputName.addEventListener('change', function () {
     cityNameValue = inputName.value
 })
-
 const getWeather = async () => {
     let finalURL = baseURL + cityNameValue + finalApiKey
     const response = await fetch(finalURL);
     try {
+
         const allData = await response.json()
         const temperature = Number(allData.main.temp);
-        tempText.innerHTML = Math.trunc(temperature - 272.15) + ' C°';
-        // console.log(temperature)
+        const { name, coord, main, sys, weather } = allData;
+        const { lon, lat } = coord;
+        const { temp_min, temp_max, humidity } = main;
+        const { country } = sys;
+
+        console.log(`Name - ${name}\nLongitude - ${lon}\nLatitude - ${lat}\nMax-Temperature - ${temp_max}\nMin-Temperature - ${temp_min}\nHumidity - ${humidity}\nWeather - ${weather[0].main}\nCountry - ${country}`);
+
+        // Appending Main Temperature
+        mainTemp.appendChild(temp).innerText = Math.trunc(temperature - 272.15) + ' C°';
     } catch (err) {
         console.log('error', err)
     }
 }
-
 generateButton.addEventListener("click", getWeather);
