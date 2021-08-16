35c5b233c6374a27ea42da1679a9950f5578922c
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2021-08-16 15:36:33 +0530

    Final Commit, LocalHost:8000

--- server.js
+++ server.js
@@ -14,15 +14,16 @@ app.use(cors());
 /* Initialize the main project folder*/
 app.use(express.static('website'));
 
-const port = 3000;
+const port = 8000;
 /* Spin up the server*/
 const server = app.listen(port, listening);
 function listening() {
     // console.log(server);
     console.log(`running on localhost: ${port}`);
+
 };
 
 // TODO-ROUTES!
-app.get('/all', (req, res) => {
-    res.send(index.html);
-})
+// app.get('/all', (req, res) => {
+//     res.send(index.html);
+// })
